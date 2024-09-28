3a103bf0b3d768b2407bf43b5f4ea3b063f0e78e
PABLO-PC <PABLO-PC@PABLO-PC>
2024-09-27 22:44:58 -0600

    init repo

---
+++ AuthServerDemoClient/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using AuthServerDemoClient.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AuthServerDemoClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Redirect(string returnUrl = "/")
+        {
+
+            return Challenge(
+                new AuthenticationProperties
+                { 
+                    RedirectUri = returnUrl 
+                },
+                "custom" 
+                );
+        }
+
+    }
+}
---
+++ AuthServerDemoClient/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AuthServerDemoClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
